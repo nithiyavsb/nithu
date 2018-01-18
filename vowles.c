77afbeab5876875153b3e318548ad2664dd29155
nithiyavsb <31506096+nithiyavsb@users.noreply.github.com>
2018-01-18 10:49:10 +0000

    Create vowles.c

---
+++ vowles.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+main()
+{
+charletter;
+scanf("%d",&letter);
+printf("enter the letter");
+switch(letter);
+case a:
+printf("enter is vowels");
+break:
+case e:
+printf("enter is vowels");
+break:
+case i:
+printf("enter is vowels");
+break:
+case o:
+printf("enter is vowles");
+break:
+case u:
+printf("enter is vowles");
+break:
+default
+printf(" it is not the vowles");
+break:
+}
