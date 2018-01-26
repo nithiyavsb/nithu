418e5c043b8b0aec86b3d66d38c48474494c6323
nithiyavsb <31506096+nithiyavsb@users.noreply.github.com>
2018-01-26 15:51:34 +0530

    Create base& hight of triangle.c

---
+++ base& hight of triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+float base, hight, triangle;
+/*input base hight of triangle*/
+scanf("%f",& base);
+printf("enter base of thr triangle:");
+scanf("%f", &hight);
+printf("enter hight of the triangle:");
+/*calculate the area of trianglae*/
+area=(base*hight)/2
+printf("area  of the triangle =%2f sq. unit",area);
+retern 0;
+}
