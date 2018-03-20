c58b6f5c4c647779b9288d7ba24ba155c6b287d0
nithiyavsb <31506096+nithiyavsb@users.noreply.github.com>
2018-03-20 15:53:13 +0530

    Create bienner .22.c

---
+++ bienner .22.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int  main()
+{
+int a[4]={1,2,3};
+int i,n;
+printf("enter the value n");
+scanf("\n%d",&n);
+for(i=0;i<4;i++)
+printf("\na[3]=%d",a[i]);
+printf("\n%d",n);
+return 0;
+}
